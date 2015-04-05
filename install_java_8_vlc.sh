830c061f6489ada0bafe0b03c23bf15c97263962
Alexandre Kervadec <alexandre.kervadec@gmail.com>
2015-04-05 22:09:23 +0200

    Added scripts pour initialisation du répertoire et installation sur Unix

---
+++ install_java_8_vlc.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+echo "Java 8 installation :"
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java8-installer
+sudo apt-get install vlc
+echo "You are now ready to go !"
